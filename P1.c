d4172862caca902bcb5a53cde8a7b836c4693f64
Reena patel <r4901964@gmail.com>
2026-02-08 18:45:30 +0530

    Add files via upload
    Github project

---
+++ P1.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main;
+{
+    printf("\n Arithmathic operation");
+        int a;
+        int b;
+        printf("\nenter value of a and b ");
+        scanf("%d,%d",&a,&b);
+        printf("\n sum is %d",a+b);
+        printf("\n subtract is %d",a-b);
+        printf("\n multiply is %d",a*b);
+        printf("\n divion is %d",a/b);
+        printf("\n reminder is %d",a%b);
+        return 0;
+
+
+}
