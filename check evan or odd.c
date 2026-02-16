2d9d2009d666500f54497bd69e7a5169b66245b1
Reena patel <r4901964@gmail.com>
2026-02-16 22:05:00 +0530

    Add files via upload

---
+++ check evan or odd.c
@@ -0,0 +1,17 @@
+check evan or odd
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
+
+    return 0;
+}
